5ff113b2910938229a473128548822e738d0e5ad
Muhammad Sannan Sherzada <24pwbcs1177@uetpeshawar.edu.pk>
2026-06-13 20:38:25 +0500

    updated the vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,11 +14,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  preview: {
-    host: "0.0.0.0",
-    port: 8080,
-    allowedHosts: "all",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
