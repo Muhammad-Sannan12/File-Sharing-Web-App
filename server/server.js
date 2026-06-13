8f94958b3c37eab858d53ce1145ba1abde73c601
Muhammad Sannan Sherzada <24pwbcs1177@uetpeshawar.edu.pk>
2026-06-13 22:00:10 +0500

    updated cors

--- server/server.js
+++ server/server.js
@@ -29,7 +29,6 @@ const routes = require("./routes");
 console.log("Loaded routes successfully");
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 const httpServer = createServer(app);
